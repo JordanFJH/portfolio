db94f4cf35f90eedde56220759d77ee8aa28bdc7
Jordan <higgsfj@gmail.com>
2024-01-31 21:43:13 -0500

    first commit of all files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div>
+      <nav><h1>Jordan higgs</h1></nav>
+      <h1>The web coder master</h1>
+      <a href="https://github.com/JordanFJH?tab=repositories">Full Git link</a>
+
+    </div>
+  )
+}
+
+export default App
